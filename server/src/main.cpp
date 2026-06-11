1ac474884d1519dc308f30a55a05cf45f5ef8419
Harun Khan <harunage@gmail.com>
2026-06-10 20:43:13 -0500

    Preliminary code for server/client

---
+++ server/src/main.cpp
@@ -0,0 +1,9 @@
+/* Server Main Code */
+
+#include "server.hpp"
+
+int main(){
+
+    return 0;
+
+}
---
+++ server/src/server.cpp
@@ -0,0 +1,21 @@
+#include "server.hpp"
+
+
+class Server{
+private:
+    const int max_threads = MAX_THREADS
+public:
+
+    /* Server Constructor */
+    Server(){
+        ;
+    }
+
+    /* Server Destructor */
+    ~Server(){
+        ;
+    }
+
+
+
+};
